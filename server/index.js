5211db8a55fa4338fd18cc226d20c50ef64b46d8
Gokulkrishnan3113 <gokulkrishnan3113@gmail.com>
2024-02-25 17:24:13 +0530

    backend task done

---
+++ server/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import cookieParser from "cookie-parser";
+import multer from "multer";
+import cors  from "cors";
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+  res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+app.use(cors({origin: "http://localhost:3000",}));
+app.use(express.json());
+app.use(cookieParser());
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post("/api/upload", upload.single("file"), function (req, res) {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/search", userRoutes);
+app.use("/api/posts", postRoutes);
+
+
+app.listen(8800, () => {
+  console.log("Connected!");
+});
